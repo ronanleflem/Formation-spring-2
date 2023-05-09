120005489e65d0a47be1ab78b29be35fb60c2c2e
Ronan LEFLEM <rleflem@consultants-solutec.fr>
2023-05-09 15:07:37 +0200

    initializer

---
+++ src/main/java/com/formation/partie2/Partie2Application.java
@@ -0,0 +1,13 @@
+package com.formation.partie2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Partie2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Partie2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/formation/partie2/Partie2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formation.partie2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Partie2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
