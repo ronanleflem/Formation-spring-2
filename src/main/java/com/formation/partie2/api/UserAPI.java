93e0604c2c82f4eabf8ba8865089481c5698bbd7
Ronan LEFLEM <rleflem@consultants-solutec.fr>
2023-05-12 09:42:12 +0200

    MDR

--- src/main/java/com/formation/partie2/api/UserAPI.java
+++ src/main/java/com/formation/partie2/api/UserAPI.java
@@ -25,7 +25,7 @@ public class UserAPI {
     }
 
     @GetMapping("/getAllUsers")
-    public List<User> getAllUsers() {
+    public List<User> getAllUsers() {dzdzadd
         ResponseEntity<List<User>> response = restTemplate.exchange(
                 BASE_URL+ "/" +"user/getAllUsers",
                 HttpMethod.GET,
