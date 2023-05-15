6491c1a29c8888b8fb105f38642f46f1722cb8d9
ronanleflem <rleflem@consultants-solutec.fr>
2023-05-15 09:59:10 +0200

    test

--- src/main/java/com/formation/partie2/Partie2Application.java
+++ src/main/java/com/formation/partie2/Partie2Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Partie2Application {
-test
+
 	public static void main(String[] args) {
 		SpringApplication.run(Partie2Application.class, args);
 	}
